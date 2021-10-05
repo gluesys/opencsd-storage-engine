d63c6bff71f7168e37b615a5568bc93f82c29e16
KimChanHyup <atyx300@naver.com>
2021-10-05 14:28:46 +0900

    first commit

---
+++ CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /root/mysql-8.0.19/storage/keti
+# Build directory: /root/mysql-8.0.19/storage/keti
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
